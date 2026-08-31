5439894591ef7acc55f438e5870bba352c3e9ea9
McCamster <cdgate05@louisville.edu>
2026-08-31 09:51:49 -0400

    Updated August 31st

---
+++ SyntaxDemo/src/main/java/github/McCamster/shadow.java
@@ -0,0 +1,34 @@
+package github.McCamster;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class shadow {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        int a = 2146483648;
+        int neg_a = -2147483648;
+        short b = 32768;
+        short neg_b = -32768;
+        long c = 2147483648L;
+        byte d = 127;
+        byte neg_d = -128;
+
+        double e = 3.1415;
+        float f = 3.141f;
+
+        int [] int_array = {1,2,3,4,5,6,7};
+        for (int i = 0; i < int_array.length; i++) {
+            System.out.println(int_array[i]);
+
+        }
+        for (int i : int_array) {
+            System.out.println(i);
+        }
+        List<Integer> list = new ArrayList<>();
+        }
+        }
+    }
